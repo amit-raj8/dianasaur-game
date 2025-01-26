e685f418367a54a52e5795bd1e16a2249918e920
amit-raj8 <amitrajj8092@gmail.com>
2025-01-26 23:53:56 +0530

    updating

--- script.js
+++
@@ -1,8 +0,0 @@
-document.onkeydown = function(e){
-    console.log("Key code is: ", e.keyCode)
-    console.log()
-    if(e.keyCode==38){
-        dino = document.querySelector('.dino');
-        dino.classList.add('animateDino');
-    }
-}
