d599cd3c3348503ea70ec82287ee3b06da197330
amit-raj8 <amitrajj8092@gmail.com>
2025-02-17 21:34:51 +0530

    fixing dino

--- script.js
+++ script.js
@@ -4,5 +4,8 @@ document.onkeydown = function(e){
     if(e.keyCode==38){
         dino = document.querySelector('.dino');
         dino.classList.add('animateDino');
+        setTimeout(() => {
+            dino.classList.remove('animateDino')
+        }, 700);
     }
 }
